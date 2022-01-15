ff8094066d339ff84d545b37af02b2463767cb13
surazzpande <surajpande20554@gmail.com>
2022-01-15 12:15:53 +0545

    first commit

--- Intern/settings.py
+++ Intern/settings.py
@@ -83,14 +83,11 @@ AUTH_USER_MODEL = 'EhatBazzar.Account'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'EhatBazzar',  
-    'HOST': 'localhost',
-    'PORT': '3306',
-    'USER': 'admin',
-    'PASSWORD': 'admin',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-}
+
 
 
 # Password validation

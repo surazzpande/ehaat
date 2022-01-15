cb12d7c637e725c25ac2220d14ec1f87393c7a45
surazzpande <surajpande20554@gmail.com>
2022-01-15 11:15:04 +0545

    first commit

--- Intern/settings.py
+++ Intern/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i4g9@n+p)v6-=ch)6#*ux4_h^-wvjun8ri9x3%!8n8pe*%naqo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','e-haat.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','e-haat.herokuapp.com']
 
 
 # Application definition
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Intern.wsgi --log-file=-
